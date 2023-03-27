99e53a8ba74dbe391005bb79094655d7edc6529d
manuel-larbi <emmanuel.larbi@amalitech.com>
2023-03-27 16:32:17 +0000

    signup: updated register route

---
+++ app/Http/Requests/RegisterRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Password;
+
+class RegisterRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\Rule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'fullname' => ['required','string','max:50','regex:/^[a-zA-Z0-9]/i'],
+            'username' => ['required','string','max:50','regex:/^[a-zA-Z0-9]/i'],
+            'employee_id' => ['required','string','max:50','regex:/^[a-zA-Z0-9]/i'],
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ];
+    }
+}
