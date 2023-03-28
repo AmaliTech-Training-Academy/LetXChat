11f9d24a98796fd0c86afc803ab725756f575ff0
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-03-28 12:51:57 +0000

    Bug: Add margin to Logo text

--- src/components/Landing-Page/Footer.jsx
+++ src/components/Landing-Page/Footer.jsx
@@ -11,7 +11,7 @@ function Footer() {
     <div className='w-full h-[455px] bg-[#755D57E5] px-24 flex items-center justify-center text-white'>
         <div className='w-full flex justify-between'>
             <div>
-                <div className='font-semibold text-5xl'>Logo</div>
+                <div className='font-semibold text-5xl mb-2'>Logo</div>
                 <span className=' text-xl'>Lorem ipsum dolor sit amet<br/> consectetur adipiscing </span>
                 <div className='flex mt-16 gap-8'>
                     {instagram_icon}
