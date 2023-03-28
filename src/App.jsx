a6c0a5106847048db1de661c881e1a0e38def489
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-03-28 15:23:20 +0000

    Feature/registrantionModal: Created registration modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import { createTheme } from "@mui/material";
 import Login from "./components/login/Login";
 import SignUp from "./components/signup/SignUp";
+import RegModal from "./components/regModal/RegModal";
 
 
 function App() {
   return (
     <div className="App">
-    
+      <RegModal />
       <SignUp />
     </div>
   );
