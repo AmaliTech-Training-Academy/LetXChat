306363842ed826f92f4acc5d116f594b1cc0e5cd
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-03-29 10:26:17 +0000

    Feature/Signup: Validated the username field to accept only numbers, letters, and underscore

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,21 +1,37 @@
+import * as yup from "yup";
 
-import * as yup from 'yup'
-
-
-const passwordRules = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/
+const passwordRules =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+const usernameRules = /^[a-zA-Z0-9_]+$/;
+const usernameUnderscoreRules = /^[a-zA-Z0-9](?:_?[a-zA-Z0-9])*[a-zA-Z0-9]$/;
 
 export const basicSchema = yup.object().shape({
-    image: yup.mixed().required('This field is required').test('fileFormat', 'Unsupported file format', (value) => {
-        if (value) {
-          return ['image/png', 'image/jpeg', 'image/jpg'].includes(value.type);
-        }
-        return true;
-      }),
-    name: yup.string().required('This field is required'),
-    employeeID: yup.string().required('This field is required'),
-    username: yup.string().required('This field is required'),
-    mail: yup.string().email('Invalid email').required('This field is required'),
-    password: yup.string().min(8).matches(passwordRules, {message: 'Please create a stronger password'}).required('This field is required'),
-    confirmPassword: yup.string().oneOf([yup.ref('password'), null], "Passwords must match").required('This field is required')
-
-})
+  image: yup
+    .mixed()
+    .required("This field is required")
+    .test("fileFormat", "Unsupported file format", (value) => {
+      if (value) {
+        return ["image/png", "image/jpeg", "image/jpg"].includes(value.type);
+      }
+      return true;
+    }),
+  name: yup.string().required("This field is required"),
+  employeeID: yup.string().required("This field is required"),
+  username: yup
+    .string()
+    .matches(usernameRules, { message: " Please use only letters, numbers, and underscores" })
+    .matches(usernameUnderscoreRules, {
+      message: "Underscore should be in the middle",
+    })
+    .required("This field is required"),
+  mail: yup.string().email("Invalid email").required("This field is required"),
+  password: yup
+    .string()
+    .min(8)
+    .matches(passwordRules, { message: "Please create a stronger password" })
+    .required("This field is required"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
+    .required("This field is required"),
+});
