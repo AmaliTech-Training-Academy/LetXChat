ede94269e56780d7cfc17b306de408938a45210d
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-04-03 11:18:43 +0000

    Feature/ChatPage: created chat page component

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,16 @@ import SignUp from "./components/signup/SignUp";
 import RegModal from "./components/regModal/RegModal";
 
 import Home from "./pages/Home";
+import ChatPage from "./pages/chatPage/ChatPage";
 
 function App() {
   return (
     <div classfullname="App">
       {/* <RegModal /> */}
-      <SignUp />
+      {/* <SignUp /> */}
       {/*  <Home /> */}
       {/* <Login />     */}
+      <ChatPage />
     </div>
   );
 }
---
+++ src/pages/chatPage/ChatPage.jsx
@@ -0,0 +1,16 @@
+import { Box, styled } from '@mui/material'
+import React from 'react'
+
+const Container = styled(Box)({
+    width: '60vw',
+    marginInline: 'auto',
+    background: 'blue'
+})
+
+const ChatPage = () => {
+  return (
+    <div>ChatPage</div>
+  )
+}
+
+export default ChatPage
