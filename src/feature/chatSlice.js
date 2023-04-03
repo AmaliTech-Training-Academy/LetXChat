b4b7a38508b32f08a90f5d378e372af6678a47b4
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-04-03 09:22:48 +0000

    Feature/Signup: Configured Redux store, reducer and base url for the backend API

--- src/feature/chatSlice.js
+++ src/feature/chatSlice.js
@@ -13,18 +13,12 @@ const authSlice = createSlice({
     registerSuccess: (state, action) => {
       state.isAuthenticated = true;
       state.user = action.payload.user;
-      state.token = action.payload.token;
       state.error = null;
-      state.isLoading = false;
-      Cookies.set("token", action.payload.token);
     },
     registerFailure: (state, action) => {
       state.isAuthenticated = false;
       state.user = null;
-      state.token = null;
       state.error = action.payload;
-      state.isLoading = false;
-      Cookies.remove("token");
     },
   },
 });
