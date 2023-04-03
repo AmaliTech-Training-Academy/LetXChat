9130bf4348e1ebf0754669f2169295d9e8319e8d
manuel-larbi <emmanuel.larbi@amalitech.com>
2023-04-03 05:32:24 +0000

    dev-backend: resolved merge conflict

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -4,15 +4,38 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginRequest;
+use App\Http\Requests\RegisterRequest;
+use App\Http\Resources\RegisterResource;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Validation\ValidationException;
 
 
 class AuthController extends Controller
 {
+    public function register(RegisterRequest $request)
+    {
+
+        $request->validated($request->all());
+
+        $image = $request->file('image');
+        $path = Storage::disk('public')->put('images', $image);
+
+        $user = User::create([
+            'fullname' => $request->fullname,
+            'username' => $request->username,
+            'employee_id' => $request->employee_id,
+            'email' => $request->email,
+            'image' => $path,
+            'password' => Hash::make($request->password),
+        ]);
+
+        return new RegisterResource($user);
+    }
+
     public function login(LoginRequest $request)
     {
         $request->validated($request->all());
