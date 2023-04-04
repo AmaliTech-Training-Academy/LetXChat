f7f9709230a0f2d1e129bd1f0f7d9f7228a304a6
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-04-04 14:00:09 +0000

    feature: Add settings serach input field

--- src/components/sidebar/Search.jsx
+++ src/components/sidebar/Search.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import search from '../../assets/Search.svg'
 
-function Search() {
+function Search({settings}) {
   return (
-    <div className='w-[295px] h-9 bg-white mt-14 mx-auto flex items-center rounded-xl'>
-        <div className='w-[25px] h-[25px] ml-2'>
+    <div className={`${!settings ? 'mt-14 rounded-xl' : 'mt-0 rounded-lg'} w-[295px] h-9 bg-white mx-auto flex items-center`}>
+        {!settings && <div className='w-[25px] h-[25px] ml-2'>
             <img src={search} alt="" className='w-full h-full object-cover'/>
-        </div>
-        <input type='text' placeholder='Search...' className='h-full w-full bg-transparent px-3'/>
+        </div>}
+        <input type='text' placeholder={`${settings ? '@username' : 'Search...'}`} className='h-full w-full bg-transparent px-3'/>
     </div>
   )
 }
--- src/components/sidebar/Settings.jsx
+++ src/components/sidebar/Settings.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import UserCard from './UserCard'
 import close from '../../assets/Close.png'
+import Search from './Search'
+import { Link } from 'react-router-dom'
 
 function Settings() {
   return (
     <div className='h-screen w-[25vw] bg-[#F3F3F3BF] flex flex-col relative'>
-        <div className='w-8 h-8 bg-[#53352D] rounded-full absolute top-5 right-7 flex justify-center items-center cursor-pointer'>
+        <Link to='/sidebar' className='w-8 h-8 bg-[#53352D] rounded-full absolute top-5 right-7 flex justify-center items-center cursor-pointer'>
             <img src={close} alt="" />
-        </div>
+        </Link>
         <UserCard settings={true} />
+        <div className='w-full h-[77px] border-y mt-[61px] flex items-center'>
+            <Search settings={true}/>
+        </div>
     </div>
   )
 }
