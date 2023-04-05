42a95850a87dad5500e036557f18fe3c21a46620
MichaelAffanyi <michael.affanyi@amalitech.com>
2023-04-05 12:02:31 +0000

    feature: Add card component on admin dashboard

--- src/pages/admin/Admin.jsx
+++ src/pages/admin/Admin.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import AdminNavbar from '../../components/admin/AdminNavbar'
+import Cards from '../../components/admin/Cards'
 
 function Admin() {
   return (
     <div className='flex justify-center'>
         <div className='w-full max-w-[1640px]'>
             <AdminNavbar />
+            <Cards />
         </div>
     </div>
   )
