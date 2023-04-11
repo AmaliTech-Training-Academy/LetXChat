5d787a4ad6020981518f4325a47d3ec3a417b23b
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-04-11 14:42:18 +0000

    Feature/ChatPage: Resolving conflicts

--- src/feature/chatSlice.js
+++ src/feature/chatSlice.js
@@ -1,21 +1,41 @@
 import { createSlice } from "@reduxjs/toolkit";
+<<<<<<< HEAD
 import Cookies from "js-cookie";
 import { loginUser } from "./authActions";
+=======
+import { registerUser } from "./authActions";
+import { BASE_URL } from "../defaultValues/DefaultValues";
+import { io } from "socket.io-client";
+import Cookies from "js-cookie";
+
+const userToken = Cookies.get("userToken") || null;
+>>>>>>> 99a3f85 (Feature/ChatPage: Migrating functionalities to chatSlice)
 
 // Initialize userToken from Cookie
 const userToken = Cookies.get("userToken") ? Cookies.get("userToken") : null;
 const userInfo = Cookies.get("userInfo") ? Cookies.get("userInfo") : null;
 const initialState = {
   loading: false,
+<<<<<<< HEAD
   userInfo,
+=======
+  userInfo: {},
+>>>>>>> 99a3f85 (Feature/ChatPage: Migrating functionalities to chatSlice)
   userToken,
   error: null,
   success: false,
+
+  text: "",
 };
+
+// Chat Rooms and Messages
+const CHAT_ROOM_URL = `${BASE_URL}/chatrooms`;
+const socket = io.connect(`${CHAT_ROOM_URL}`);
 
 const authSlice = createSlice({
   name: "auth",
   initialState,
+<<<<<<< HEAD
   reducers: {},
   extraReducers: (builder) => {
     // Login User
@@ -25,6 +45,27 @@ const authSlice = createSlice({
         state.error = null;
         state.success = false;
       })
+=======
+  reducers: {
+    sendMessage: (state, action) => {
+      // const user =
+
+      const messageObj = {
+        message: state.text,
+        author: username,
+        date: new Date(),
+      };
+
+      socket.emit("send_message", { ...messageObj });
+      state.text;
+    },
+  },
+  extraReducers: {
+    [registerUser.pending]: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+>>>>>>> 99a3f85 (Feature/ChatPage: Migrating functionalities to chatSlice)
 
       .addCase(loginUser.fulfilled, (state, { payload }) => {
         state.loading = false;
@@ -41,4 +82,5 @@ const authSlice = createSlice({
   },
 });
 
+// export const { sendMessage } = chatSlice.actions;
 export default authSlice.reducer;
