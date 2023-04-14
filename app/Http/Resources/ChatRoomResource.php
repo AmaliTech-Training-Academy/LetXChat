a4c194cfe0cebb4cb4368453788cbee0ec27c178
manuel-larbi <emmanuel.larbi@amalitech.com>
2023-04-14 15:17:50 +0000

    dev-backend: updated chatrooms response data

--- app/Http/Resources/ChatRoomResource.php
+++ app/Http/Resources/ChatRoomResource.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Resources;
 
+use App\Models\ChatMessage;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -18,6 +20,18 @@ class ChatRoomResource extends JsonResource
             'id' => $this->id,
             'name' => $this->name,
             'image' => env('APP_URL').'/LetXChat/storage/app/public/'.$this->image,
+            'recent_message' => $this->getRecentMessage($this->id)
         ];
+    }
+
+    public function getRecentMessage($roomID)
+    {
+        $recent = ChatMessage::where('chat_room_id', $roomID)->latest()->first();
+        if(!$recent) return null;
+        return [
+            'message' => $recent->message,
+            'time' => Carbon::parse($recent->created_at)->format('g:i a')
+        ];
+
     }
 }
