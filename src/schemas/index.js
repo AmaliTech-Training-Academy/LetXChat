332509e45d29f82dcd6304a09b7d0b7d043655ea
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-04-14 08:33:15 +0000

    Feature/groupDetails: changed Yup to yup

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -1,4 +1,5 @@
-import * as Yup from "Yup";
+// import * as yup from "yup";
+import * as yup from 'yup'
 
 const passwordRules =
   /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
@@ -7,11 +8,11 @@ const usernameUnderscoreRules = /^[a-zA-Z0-9](?:_?[a-zA-Z0-9])*[a-zA-Z0-9]$/;
 const amalitechemail =
   /^[a-zA-Z0-9]+([-][a-zA-Z0-9]+)?(\.[a-zA-Z0-9]+)+@(amalitech\.com|amalitech\.org)$/;
 
-export const basicSchema = Yup.object().shape({
-  image: Yup.mixed().required("This field is required"),
+export const basicSchema = yup.object().shape({
+  image: yup.mixed().required("This field is required"),
 
-  fullname: Yup.string().trim().required("This field is required"),
-  username: Yup.string()
+  fullname: yup.string().trim().required("This field is required"),
+  username: yup.string()
     .trim()
     .matches(usernameRules, {
       message: " Please use only letters, numbers, and underscores",
@@ -20,26 +21,26 @@ export const basicSchema = Yup.object().shape({
       message: "Underscore should be in the middle",
     })
     .required("This field is required"),
-  email: Yup.string()
+  email: yup.string()
     .trim()
     .email("Invalid email")
     .matches(amalitechemail, {
       message: "email address must be from amalitech domain",
     })
     .required("This field is required"),
-  password: Yup.string()
+  password: yup.string()
     .trim()
     .min(8)
     .matches(passwordRules, { message: "Please create a stronger password" })
     .required("This field is required"),
-  password_confirmation: Yup.string()
-    .oneOf([Yup.ref("password"), null], "Passwords must match")
+  password_confirmation: yup.string()
+    .oneOf([yup.ref("password"), null], "Passwords must match")
     .required("This field is required"),
 });
 
 
-export const loginSchema = Yup.object().shape({
-  emailID: Yup.string().required("This field is required"),
+export const loginSchema = yup.object().shape({
+  emailID: yup.string().required("This field is required"),
 
-  password: Yup.string().required("This field is required"),
+  password: yup.string().required("This field is required"),
 });
