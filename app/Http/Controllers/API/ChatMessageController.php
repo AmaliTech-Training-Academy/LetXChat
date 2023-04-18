e5857530415aa334ce3bf5ddb4255d5ff7250edd
manuel-larbi <emmanuel.larbi@amalitech.com>
2023-04-18 14:06:54 +0000

    feature/chat-backend: added sender image

--- app/Http/Controllers/API/ChatMessageController.php
+++ app/Http/Controllers/API/ChatMessageController.php
@@ -69,18 +69,19 @@ class ChatMessageController extends Controller
         broadcast(new NewChatMessage(
             $request->message,
             'https://takoraditraining.com/LetXChat/storage/app/public/'.$image,
-            $video,
-            $audio,
-            $file
+            'https://takoraditraining.com/LetXChat/storage/app/public/'.$video,
+            'https://takoraditraining.com/LetXChat/storage/app/public/'.$audio,
+            'https://takoraditraining.com/LetXChat/storage/app/public/'.$file
         ))->toOthers();
 
         return response()->json([
             'sender' => Auth::user()->fullname,
+            'sender_image' => 'https://takoraditraining.com/LetXChat/storage/app/public/'.Auth::user()->image,
             'message' => $newMessage->message,
             'image' => 'https://takoraditraining.com/LetXChat/storage/app/public/'.$newMessage->image,
-            'video' => $newMessage->video,
-            'voiceNote' => $newMessage->voiceNote,
-            'file' => $newMessage->file
+            'video' => 'https://takoraditraining.com/LetXChat/storage/app/public/'.$newMessage->video,
+            'voiceNote' => 'https://takoraditraining.com/LetXChat/storage/app/public/'.$newMessage->voiceNote,
+            'file' => 'https://takoraditraining.com/LetXChat/storage/app/public/'.$newMessage->file
         ]);
     }
 }
