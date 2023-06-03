fcb69e78af33f94a0e36ce4a42b0afbb340d73b4
Emmanuel Kpendo <emmanuel.kpendo@amalitech.com>
2023-06-03 23:46:48 +0000

    Restyled admin login page

--- src/components/login/Login.jsx
+++ src/components/login/Login.jsx
@@ -50,15 +50,16 @@ const Login = () => {
   return (
     <main className="h-screen w-screen flex items-center justify-center">
       <form
-        autoComplete="off"
         onSubmit={handleSubmit}
-        className="w-[90vw] md:w-[550px] h-max md:h-[602px] bg-primary shadow-backgroundShadow rounded-[15px] py-5 sm:py-0 px-5 flex items-center flex-col justify-center"
+        className="w-[90vw] md:w-[550px] h-max bg-primary shadow-backgroundShadow rounded-[15px] py-8 sm:py-0 px-5 flex items-center flex-col justify-center"
       >
         <h2 className="font-[inter] font-bold text-[22.2944px] leading-[27px] text-center text-brown uppercase mb-5">
           Log In
         </h2>
 
         <section className="flex flex-col w-full md:gap-[30px] mb-2">
+          <div className="mb-4">
+
           <InputField
             type="text"
             label="Email/ Chat ID*"
@@ -81,32 +82,35 @@ const Login = () => {
             errorMessage={touched.password && errors.password}
             placeholder="Enter password"
           />
+            </div>
 
           <Button type="submit">
             {loading || isSubmitting ? "Loading..." : "Log In"}
           </Button>
 
-<div className=" flex flex-col items-center gap-3 mt-3 sm:mt-0">
-
-          <p>
-            Don't have an account?{" "}
-            <Link to="/signup" style={{ color: "#3683F5", cursor: "pointer" }}>
-              Sign Up
-            </Link>
-          </p>
-          <p>
-            Are you and Admin?{" "}
-            <Link
-              to="/admin-login"
-              style={{
-                color: "#3683F5",
-                cursor: "pointer",
-              }}
+          <div className=" flex flex-col items-center gap-3 mt-3 sm:mt-0">
+            <p>
+              Don't have an account?{" "}
+              <Link
+                to="/signup"
+                style={{ color: "#3683F5", cursor: "pointer" }}
               >
-              Login as Admin
-            </Link>
-          </p>
-              </div>
+                Sign Up
+              </Link>
+            </p>
+            <p>
+              Are you and Admin?{" "}
+              <Link
+                to="/admin-login"
+                style={{
+                  color: "#3683F5",
+                  cursor: "pointer",
+                }}
+              >
+                Login as Admin
+              </Link>
+            </p>
+          </div>
         </section>
       </form>
     </main>
